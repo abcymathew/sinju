fde74d59d0d8127bf19ab30adc91301f15ae908d
abcymathew <151534573+abcymathew@users.noreply.github.com>
2023-12-12 22:16:59 +0530

    Update Student.java

--- Aggregation/src/main/java/Aggregate/Student.java
+++ Aggregation/src/main/java/Aggregate/Student.java
@@ -19,6 +19,7 @@ public class Student {
 		System.out.println("houseno is"+" "+a.houseno);
 		System.out.println("housename is"+" "+a.housename);
 		System.out.println("pincode is"+" "+a.pincode);
+		System.out.println("hi");
 	}
 
 	public static void main(String[] args) {
